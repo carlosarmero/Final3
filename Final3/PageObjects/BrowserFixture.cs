044e67414046495ded898e1d2aa0d346d77abc08
carlos <carloch1234@hotmail.com>
2024-10-09 16:12:09 -0500

    misslogpar!

---
+++ Final3/PageObjects/BrowserFixture.cs
@@ -0,0 +1,15 @@
+﻿public class BrowserFixture : IDisposable
+{
+    public string Browser { get; private set; }
+
+    public BrowserFixture()
+    {
+        // Configura el navegador por defecto aquí
+        Browser = "chrome"; // Cambia esto si es necesario
+    }
+
+    public void Dispose()
+    {
+        // Aquí puedes manejar cualquier limpieza si es necesario
+    }
+}
